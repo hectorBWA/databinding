685352dfcc27bedbbdae68806ea5917ad455eabc
hectorBWA <hectorzavala12345@gmail.com>
2018-06-01 14:46:07 -0700

    directives branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .online{
+      color: white;
+    }
+    .standard{
+      font-size: 40px;
+    }
+  `]
 })
 export class AppComponent {
   title = 'app';
 
   allowNewServer: boolean = false;
-  serverCreateionStatus: string = '';
+  serverCreateionStatus: string = 'No server was created';
+  serverName: string;
+  serverCreated: boolean = false;
 
   constructor(){
 
@@ -27,4 +37,16 @@ export class AppComponent {
     console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
   }
+}
+
+
+
+
+
+//serv
+
+export class ServerComponent {
+
+  serverId: number = 10;
+
 }
